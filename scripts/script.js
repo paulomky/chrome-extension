4bdbc447975c4c3527ef3985408a86b59e25e4ca
Paulo Oliveira <pauloeduardo2001@hotmail.com>
2021-11-17 15:31:15 -0300

    add inputs test

--- scripts/script.js
+++ scripts/script.js
@@ -16,15 +16,3 @@ function openTopdeskPage(){
     artiaPage.style.display = "none";
     topdeskPage.style.display = "block";
 }
-
-var actualCode = '(' + function() {
-    // All code is executed in a local scope.
-    // For example, the following does NOT overwrite the global `alert` method
-    var alert = null;
-    // To overwrite a global variable, prefix `window`:
-    window.alert = null;
-} + ')();';
-var script = document.createElement('script');
-script.textContent = actualCode;
-(document.head||document.documentElement).appendChild(script);
-script.remove();
